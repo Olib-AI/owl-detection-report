a3c4f5b5164e529f7b4814175da56c345bd8691c
Akram Hasan Sharkar <ahs@akhaura.info>
2026-04-06 02:39:54 -0400

    Use http://example.com to avoid SSL cert errors in long benchmarks

--- src/benchmark.py
+++ src/benchmark.py
@@ -14,7 +14,7 @@ from .s3_uploader import s3_configured, upload_directory
 
 logger = logging.getLogger(__name__)
 
-TARGET_URL = "https://example.com"
+TARGET_URL = "http://example.com"
 ITERATIONS = 1000
 
 
